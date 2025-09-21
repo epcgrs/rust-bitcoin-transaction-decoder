530f4d3dc299653cf4453cfc1ddb442e1e4abb56
epcgrs <emmanuelf988@gmail.com>
2025-09-21 17:26:26 -0300

    study: get the right bytes and change to u32 format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #[allow(unused_variables)]
 fn read_version(transaction_hex: &str) -> u32 {
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
-    let version_bytes = &transaction_bytes[0..4];
-    println!("version bytes: {:?}", version_bytes);
-    1
+    let version_bytes: [u8; 4] = transaction_bytes[0..4].try_into().unwrap();
+    let version = u32::from_le_bytes(version_bytes);
+    version
 }
 
 #[allow(unused_variables)]
