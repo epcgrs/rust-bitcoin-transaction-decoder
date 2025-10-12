59ffde07d8abbd572097843c6e9689a9b01600a3
epcgrs <emmanuelf988@gmail.com>
2025-10-12 18:34:28 -0300

    feat: outputs reading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::io::Read;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
+struct Transaction {
+    version: u32,
+    inputs: Vec<Input>,
+    outputs: Vec<Output>
+}
+
+#[derive(Debug, Serialize)]
 struct Input {
-    txid: [u8; 32],
+    txid: String,
     output_index: u32,
-    script_sig: Vec<u8>,
+    script_sig: String,
     sequence: u32,
+}
+
+#[derive(Debug, Serialize)]
+struct Output {
+    amount: f64,
+    script_pubkey: String,
+}
+
+struct Amount(u64);
+
+impl Amount {
+    pub fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
 }
 
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
@@ -34,25 +56,30 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
    
 }
 
-
 fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
     let mut buffer = [0; 4];
     transaction_bytes.read(&mut buffer).unwrap();
     u32::from_le_bytes(buffer)
 }
 
-fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
+fn read_amount(transaction_bytes: &mut &[u8]) -> Amount {
+    let mut buffer = [0; 8];
+    transaction_bytes.read(&mut buffer).unwrap();
+    Amount(u64::from_le_bytes(buffer))
+}
+
+fn read_txid(transaction_bytes: &mut &[u8]) -> String {
     let mut buffer = [0; 32];
     transaction_bytes.read(&mut buffer).unwrap();
     buffer.reverse();
-    buffer
+    hex::encode(buffer)
 }
 
-fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
+fn read_script(transaction_bytes: &mut &[u8]) -> String {
     let script_size = read_compact_size(transaction_bytes) as usize;
     let mut buffer = vec![0_u8; script_size];
     transaction_bytes.read(&mut buffer).unwrap();
-    buffer 
+    hex::encode(buffer) 
 }
 
 fn main() {
@@ -79,8 +106,27 @@ fn main() {
         } );
     }
 
-    println!("version: {}", version);
-    println!("Inputs: {:?}", inputs);
+    let output_count = read_compact_size(&mut bytes_slice);
+    let mut outputs = vec![];
+
+    for _ in 0..output_count {
+        let amount = read_amount(&mut bytes_slice).to_btc();
+        let script_pubkey = read_script(&mut bytes_slice);
+
+        outputs.push(Output {
+            amount,
+            script_pubkey
+        });
+    }
+
+
+    let transaction = Transaction {
+        version,
+        inputs,
+        outputs
+    };
+
+    println!("{}", serde_json::to_string_pretty(&transaction).unwrap());
 }
 
 #[cfg(test)]
